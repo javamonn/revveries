229db3f7673eac7e9269439cead3368df54e1f4b
Daniel Ramirez <javamonn@gmail.com>
2015-05-31 13:59:38 -0400

    setup rails

---
+++ revveries_cms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevveriesCms::Application.config.secret_token = '95c8fa686208417075d4fa4ca55907dd2ae89ca37a33fee1eae5f6fd88813f3cacf5917072377f33f35cd23bfb9b2f7e487508159625527cc3581e95067b44ce'
