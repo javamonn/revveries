51361392807400b4c2ce338f817b9f7a306d57ce
Daniel Ramirez <javamonn@gmail.com>
2015-05-31 14:12:57 -0400

    setup locomotive

---
+++ revveries_cms/config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret 'ae4df8f03e3a49a51b4c526a2181f2ee196d1eb33b5bf423c305a15d8d7721bc'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
